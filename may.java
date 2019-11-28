c68f8a802d022ea75b861944c31ddb687f043822
mcmayur09 <50124275+mcmayur09@users.noreply.github.com>
2019-11-28 23:21:22 +0530

    Add files via upload

---
+++ may.java
@@ -0,0 +1,21 @@
+package may;
+public class may{
+  public void println(String s){
+    System.out.println(s);
+  }
+  public void println1(int x){
+    System.out.println(x);
+  }
+  public void print(String s){
+    System.out.print(s);
+  }
+  public void print1(int x){
+    System.out.print(x);
+  }
+  public void printlnc(char c){
+    System.out.println(c);
+  }
+  public void printc(char c){
+    System.out.print(c);
+  }
+}
